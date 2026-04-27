e563306357f9f93968b06b55929ddf3c614bbc32
LuizCout <rezendecoutinholuiz@gmail.com>
2026-04-27 16:59:50 -0300

    Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -9,3 +9,10 @@ def rolar_dados(n):
         dados_rolados.append(dado)
         i+=1
     return dados_rolados
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dado_guardar = dados_rolados[dado_para_guardar]
+    dados_no_estoque.append(dado_guardar)
+    dados_rolados.pop(dado_para_guardar)
+    lista_final = [dados_rolados, dados_no_estoque]
+    return lista_final
