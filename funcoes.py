8614d1fd1d94ee33c46990ed933fd5c6355da55a
femastandrea <fellipeam@al.insper.edu.br>
2026-04-27 16:54:45 -0300

    Rolar dados

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,11 @@
-aaaaaa bbbbbbbbbbb
+#Rolar Dados
+import random
+
+def rolar_dados(n):
+    dados_rolados = []
+    i = 1
+    while i <= n:
+        dado = random.randint(1,6)
+        dados_rolados.append(dado)
+        i+=1
+    return dados_rolados
